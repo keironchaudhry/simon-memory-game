a304bcdcc66295488a5e4aa5ab77746775f2aa4c
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-08-01 07:21:26 +0000

    Add in relevant functions for walkthrough video and use jest testing in increments

---
+++ scripts/tests/game.test.js
@@ -0,0 +1,51 @@
+
+const { game, newGame, showScore } = require("../game");
+
+/** To be written at the beginning for every HTML file that is to be loaded into the DOM  */
+beforeAll(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
+
+describe("game object contains correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true);
+    });
+    test("currentGame key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+    test("playerMoves key exists", () => {
+        expect("playerMoves" in game).toBe(true);
+    });
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+    });
+    test("choices contains correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+});
+
+describe("newGame works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        game.playerMoves = [1, 2, 3];
+        game.currentGame = [1, 2, 3];
+        document.getElementById("score").innerText = "42";
+        newGame();
+    });
+    test("should set game score to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+    test("should clear playerMoves array", () => {
+        expect(game.playerMoves).toEqual([]);
+    });
+    test("should clear currentGame array", () => {
+        expect(game.currentGame).toEqual([]);
+    });
+    test("should display 0 for the element with an id of score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
+    });
+});
