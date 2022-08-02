67083685e3f0649455223edab47bc1a690904f4f
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-08-02 12:03:25 +0000

    CrModify and test functionality of newgame function

--- scripts/game.js
+++ scripts/game.js
@@ -1,21 +1,29 @@
 let game = {
-    score: 0,
     currentGame: [],
     playerMoves: [],
+    score: 0,
     turnNumber: 0,
-    choices: ["button1", "button2", "button3", "button4"],
-}
+    choices: ["button1", "button2", "button3", "button4"]
+};
 
 function newGame() {
-    game.score = 0;
-    game.playerMoves = [];
     game.currentGame = [];
+    game.playerMoves = [];
+    game.score = 0;
+
+    for (let circle of document.getElementsByClassName("circle")) {
+        if (circle.getAttribute("data-listener") !== "true") {
+            circle.addEventListener("click", (e) => {
+                let move = e.target.getAttribute("id");
+                lightsOn(move);
+                game.playerMoves.push(move);
+                playerTurn();
+            });
+            circle.setAttribute("data-listener", "true");
+        }
+    }
     showScore();
     addTurn();
-}
-
-function showScore() {
-    document.getElementById("score").innerText = game.score;
 }
 
 function addTurn() {
@@ -24,22 +32,26 @@ function addTurn() {
     showTurns();
 }
 
-function lightsOn(circ) {
-    document.getElementById(circ).classList.add("light");
-    setTimeout(() => {
-        document.getElementById(circ).classList.remove("light");
-    }, 400);
-}
-
 function showTurns() {
     game.turnNumber = 0;
-    let turns = setInterval(() => {
+    let turns = setInterval(function () {
         lightsOn(game.currentGame[game.turnNumber]);
         game.turnNumber++;
         if (game.turnNumber >= game.currentGame.length) {
             clearInterval(turns);
         }
     }, 800);
+}
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add(circ + "light");
+    setTimeout(function () {
+        document.getElementById(circ).classList.remove(circ + "light");
+    }, 400);
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
 }
 
 module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
